e968f3e5247e610b1dac5b2115e45816b1d2e6cc
christiangrossi <christian.grossi@outlook.com>
2018-08-01 16:26:37 -0300

    criados métodos de validação de campos de formulário (FormGroup)

--- src/app/shared/utils/util.input.ts
+++ src/app/shared/utils/util.input.ts
@@ -5,28 +5,27 @@ export class UtilInput {
   constructor() { }
 
   /**
-   *
-   * @param formGroup
-   * @param propFormName
+   * Verifica e retira espaços em branco do campo
+   * @param formGroup formGroup cujo campo será verificado
    */
   static trim(formGroup: FormGroup, propFormName: string) {
     formGroup.get(propFormName).setValue(formGroup.get(propFormName).value.trim());
-    console.log(formGroup.controls);
   }
 
-
+  /**
+   * Retorna um array de strings representando as propriedades do formGroup
+   * @param formGroup formGroup do qual as propiredades serão listadas no array
+   */
   static verificarCampos(formGroup: FormGroup) {
-    Object.keys(formGroup.controls).map(field => {
-    //  formGroup.get(field).invalid
-
-    });
+    return Object.keys(formGroup.controls);
   }
 
 
   /**
-   *
-   * @param formGroup
-   * @param propFormName
+   * Verifica se o campo de telefone/celular não contém espaços
+   * caso haja espaços, o conteúdo do campo será apagado
+   * @param formGroup formGroup do qual será obtido o parâmetro para verificação
+   * @param propFormName nome do parâmetro do formGroup
    */
   static verificarPreenchimentoDeTelefone(formGroup: FormGroup, propFormName: string) {
     const value = formGroup.get(propFormName).value;
